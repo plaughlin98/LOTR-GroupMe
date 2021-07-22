a8791b4258c8478e07a075379706d202d4e72e2b
plaughlin98 <plaughlin98@gmail.com>
2021-07-22 11:06:40 -0500

    Text file for past messages

--- api_config.py
+++ api_config.py
@@ -3,14 +3,13 @@ import requests
 
 
 class LotrAPI:
-    def __init__(self, URL, headers, end_point):
+    def __init__(self, URL, headers):
         self.URL = URL
         self.headers = headers
-        self.end_point = end_point
         
     
     def get_data(self, end_point):
-        request = requests.get(self.URL + '/' + self.end_point, headers=self.headers).json()
+        request = requests.get(self.URL + '/' + end_point, headers=self.headers).json()
         response = request['docs']
         return response
 
